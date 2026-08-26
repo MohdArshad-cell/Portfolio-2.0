4f070fa2adde5b8bfdff3ec93887001f1a1daeff
MohdArshad-cell <arshadmohd8574@gmail.com>
2026-08-26 15:19:48 +0530

    Change projects grid to 2 columns to balance 4 items

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -74,7 +74,7 @@ export default function Projects() {
 
         <GitHubStats />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {projects.map((p, i) => (
             <ProjectCard key={i} project={p} index={i} />
           ))}
